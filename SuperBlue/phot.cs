c4f956138a6ecec082041d5c5841d97ec9a2ff13
YusukeKaneda <32565370+YusukeKaneda@users.noreply.github.com>
2019-03-21 05:26:13 +0900

    VRで風景を楽しむゲームSuperBlue
    写真撮影してデータを外部に保存するスクリプト
    撮影した写真を表示するスクリプト

---
+++ SuperBlue/phot.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+public class phot : MonoBehaviour {
+	public RenderTexture rt;	//カメラディスプレイ
+	public static int a = 0;	//シャッターカウント
+	public float ct;			//残りフィルム
+	public Text film;			//残りフィルム表示テキスト
+	// Use this for initialization
+	void Start () {
+		a = 0;														//カウント初期化		
+		DirectoryInfo di = Directory.CreateDirectory(@"C:\phot");	//PCのC直下に写真保存用フォルダphotを作成
+	}
+		
+	void Update () {
+		SteamVR_TrackedObject trackedObject = GetComponent<SteamVR_TrackedObject>();
+		var device = SteamVR_Controller.Input((int)trackedObject.index);				//SteamVRのコントローラーを定義
+
+		if (a < 20) {	//20枚まで撮影可能
+			
+			if (Input.GetKeyDown ("t") || device.GetPressDown (SteamVR_Controller.ButtonMask.Touchpad)) { //キーボートのtもしくはVRコントローラーのタッチパッド押し込み入力
+				Take ();		
+				a++;
+				ct = 20 - a;
+				film.GetComponent<Text> ().text = "残り枚数" + ct.ToString ("00") + "枚";　//残りフィルムを表示
+			}
+		} 
+	}
+
+	void Take()		//写真撮影メソッド
+	{
+		Texture2D tex = new Texture2D(rt.width, rt.height, TextureFormat.RGB24, false);
+		RenderTexture.active = rt;
+		tex.ReadPixels(new Rect(0, 0, rt.width, rt.height), 0, 0);
+		tex.Apply();
+		byte[] bytes = tex.EncodeToPNG();							//保温する写真データを定義
+		Object.Destroy(tex);
+
+		{
+			File.WriteAllBytes(@"C:\phot/phot" + a + ".png", bytes);	//C:/phot/ 上にphot0~19.pngを保存
+		}
+	}
+}
---
+++ SuperBlue/show.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class show : MonoBehaviour
+{
+	int i;	//カウント用変数
+	public Renderer screen;		//撮影した写真表示用
+	string[] url = new string[20];	//撮影した写真の絶対パスを入れる用のstring型変数
+	void Start()
+	{
+		for (i = 0; i < phot.a; i++) {
+			url [i] = "file://C:/phot/phot" + i + ".png";	//string配列に撮影した分0~19のデータの絶対パスを保存
+		}
+		WWW www = new WWW (url [0]);
+		screen = GetComponent<Renderer> ();
+		screen.material.mainTexture = www.texture;			//起動時に最初に撮影したデータを表示
+		i = 1;												//初期化
+	}
+
+
+	void Update()
+	{
+		SteamVR_TrackedObject trackedObject = GetComponent<SteamVR_TrackedObject>();
+		var device = SteamVR_Controller.Input((int)trackedObject.index);				//SteamVRのコントローラーを定義
+
+		if (Input.GetMouseButtonDown (0) || device.GetPressDown (SteamVR_Controller.ButtonMask.Touchpad)) {    //マウスを左クリックもしくはVRコントローラーのタッチパッド押し込み入力
+			if (i < phot.a) {
+				WWW www = new WWW (url [i]);
+				screen.material.mainTexture = www.texture;		//撮影したデータ撮影した分まで表示
+				i++;
+			} else {
+				SceneManager.LoadScene ("titlle");				//表示し終わったらスタート画面にもどる
+			}
+		}
+	}
+}
