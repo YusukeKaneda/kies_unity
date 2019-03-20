1c2a2b924adaac8cd5cd14da689f13a98e6084d3
YusukeKaneda <32565370+YusukeKaneda@users.noreply.github.com>
2019-03-21 05:24:28 +0900

    NT加賀に出展したVRピラミッドジェットコースター
    ゲームを終了する当たり判定のみ

---
+++ NTKaga/end_trigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class end_trigger : MonoBehaviour {
+    public GameObject text;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player"){
+            text.GetComponent<owari>().enabled = true;
+        }
+    }
+}
